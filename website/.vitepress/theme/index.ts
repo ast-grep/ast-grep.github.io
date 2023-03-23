6f424c8022a7f0f5308736c7ac23e04e2031df47
HerrintonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-03-22 23:54:16 -0700

    feat: add sponsors to homepage

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,4 +1,16 @@
+import {h} from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-export default DefaultTheme
+const sponsorImg = 'https://raw.githubusercontent.com/HerringtonDarkholme/sponsors/main/sponsorkit/sponsors.svg'
+
+export default {
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'home-features-after': () => h(
+        'div', {style: { margin: 'auto', width: 'fit-content'}},
+          h('img', {src: sponsorImg})),
+    })
+  },
+}
