417ba4a749f7b0f620450a8a7cb88302bb9865b8
HerrintonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-03-24 09:52:12 -0700

    Revert "feat: add sponsors to homepage"
    This reverts commit 6f424c8022a7f0f5308736c7ac23e04e2031df47.

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -1,16 +1,4 @@
-import {h} from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-const sponsorImg = 'https://raw.githubusercontent.com/HerringtonDarkholme/sponsors/main/sponsorkit/sponsors.svg'
-
-export default {
-  ...DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'home-features-after': () => h(
-        'div', {style: { margin: 'auto', width: 'fit-content'}},
-          h('img', {src: sponsorImg})),
-    })
-  },
-}
+export default DefaultTheme
