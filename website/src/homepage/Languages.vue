3d4dc4f4db6054c9834565d1aadf872e5691d3bc
HerrintonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2023-11-10 14:12:57 -0800

    feat: add links to example pages in homepage

--- website/src/homepage/Languages.vue
+++ website/src/homepage/Languages.vue
@@ -3,15 +3,23 @@
     <h1>Supported Languages</h1>
   </div>
   <div class="languages">
-    <img src='/icons/C.svg'/>
+    <a href="/catalog/c/">
+      <img src='/icons/C.svg'/>
+    </a>
     <img src='/icons/CSharp.svg'/>
     <img src='/icons/Go.svg'/>
     <img src='/icons/Java.svg'/>
     <img src='/icons/JS.svg'/>
     <img src='/icons/Kotlin.svg'/>
-    <img src='/icons/Python.svg'/>
-    <img src='/icons/Rust.svg'/>
-    <img src='/icons/TS.svg'/>
+    <a href="/catalog/python/">
+      <img src='/icons/Python.svg'/>
+    </a>
+    <a href="/catalog/rust/">
+      <img src='/icons/Rust.svg'/>
+    </a>
+    <a href="/catalog/typescript/">
+      <img src='/icons/TS.svg'/>
+    </a>
   </div>
 </template>
 <style scoped>
