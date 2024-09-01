9905e2b83c1f32fb3d061756d48aaa59840a760e
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-08-31 20:38:31 -0400

    fix: fix typing

--- website/src/components/Playground.vue
+++ website/src/components/Playground.vue
@@ -9,7 +9,7 @@ import { showToast } from './utils/Toast.vue'
 import Toolbars from './Toolbars.vue'
 import EnvDisplay from './EnvDisplay.vue'
 import '../style.css'
-import { initializeParser, setGlobalParser, doFind } from './lang'
+import { initializeParser, setGlobalParser, doFind, Match } from './lang'
 import { restoreState, Mode as ModeImport } from '../state'
 import { langLoadedKey } from './dump/dumpTree'
 
@@ -36,22 +36,23 @@ function changeActiveEditor(active: string) {
   activeEditor.value = active
 }
 
-let yaml = shallowRef(null)
+type YAML = typeof import('js-yaml')
+let yaml = shallowRef<YAML | null>(null)
 import('js-yaml').then(yml => {
   yaml.value = yml
 })
 
-const matchedHighlights = shallowRef([])
-const matchedEnvs = shallowRef([])
+const matchedHighlights = shallowRef([] as Match[])
+const matchedEnvs = shallowRef([] as unknown[])
 const rewrittenCode = shallowRef(source.value)
-const ruleErrors = shallowRef<string | null>(null)
+const ruleErrors = shallowRef<string>()
 
 function parseYAML(src: string) {
   return yaml.value!.loadAll(src) as any[]
 }
 
 function buildRules() {
-  let json
+  let json = []
   if (mode.value === Mode.Patch && query.value) {
     json = [{
       id: 'test-rule',
@@ -67,7 +68,7 @@ function buildRules() {
     }]
   } else if (config.value) {
     const ruleStr = config.value // make sync access
-    json = parseYAML(ruleStr) as unknown[]
+    json = parseYAML(ruleStr) as any[]
     let i = 0
     for (let rule of json) {
       if (!rule.id) {
@@ -99,8 +100,8 @@ watchEffect(async (onInvalidate) => {
     rewrittenCode.value = fixed
     matchedHighlights.value = matches
     matchedEnvs.value = matches.map(m => m.env)
-    ruleErrors.value = null
-  } catch (e) {
+    ruleErrors.value = undefined
+  } catch (e: any) {
     if (invalidated) { return }
     console.error(e)
     ruleErrors.value = e.toString()
