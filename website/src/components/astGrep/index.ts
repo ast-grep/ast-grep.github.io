c76497e3ac6a22c077fc152bb8beda54c202ab08
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-09-01 17:52:22 -0400

    refactor: extract doFind

--- website/src/components/astGrep/index.ts
+++ website/src/components/astGrep/index.ts
@@ -1,6 +1,6 @@
-import { shallowRef, watchEffect } from 'vue'
+import { shallowRef, watchEffect, Ref } from 'vue'
 import { doFind, Match, useSetupParser } from './lang'
-import { Mode, useSgState } from './state'
+import { Mode, State, useSgState } from './state'
 
 export type { SupportedLang, Match } from './lang'
 export { initializeParser, langLoadedKey } from './lang'
@@ -10,58 +10,45 @@ type YAML = typeof import('js-yaml')
 
 const yamlImport = import('js-yaml')
 
-// NB: this hook cannot be async since it sets up `provide`
-export function useAstGrep() {
-  const {
-    state,
-    lang,
-    ...refs
-  } = useSgState()
+function buildRules(yaml: YAML, state: State) {
+  let json = []
+  if (state.mode === Mode.Patch && state.query) {
+    json = [{
+      id: 'test-rule',
+      language: state.lang,
+      rule: {
+        pattern: {
+          context: state.query,
+          strictness: state.strictness || undefined,
+          selector: state.selector || undefined,
+        },
+      },
+      fix: state.rewrite || '',
+    }]
+  } else if (state.config) {
+    const ruleStr = state.config // make sync access
+    json = yaml.loadAll(ruleStr) as any[]
+    let i = 0
+    for (let rule of json) {
+      if (!rule.id) {
+        rule.id = `test-rule-${i++}`
+      }
+      rule.language = state.lang
+    }
+  }
+  return json
+}
 
-  const langLoaded = useSetupParser(lang)
-
-  let yaml = shallowRef<YAML | null>(null)
-  yamlImport.then(yml => {
-    yaml.value = yml
-  })
-
+function useDoFind(langLoaded: Ref<boolean>, state: State) {
   const matchedHighlights = shallowRef([] as Match[])
   const matchedEnvs = shallowRef([] as unknown[])
   const rewrittenCode = shallowRef(state.source)
   const ruleErrors = shallowRef<string>()
 
-  function parseYAML(src: string) {
-    return yaml.value!.loadAll(src) as any[]
-  }
-
-  function buildRules() {
-    let json = []
-    if (state.mode === Mode.Patch && state.query) {
-      json = [{
-        id: 'test-rule',
-        language: lang.value,
-        rule: {
-          pattern: {
-            context: state.query,
-            strictness: state.rewrite || undefined,
-            selector: state.selector || undefined,
-          },
-        },
-        fix: state.rewrite || '',
-      }]
-    } else if (state.config) {
-      const ruleStr = state.config // make sync access
-      json = parseYAML(ruleStr)
-      let i = 0
-      for (let rule of json) {
-        if (!rule.id) {
-          rule.id = `test-rule-${i++}`
-        }
-        rule.language = lang.value
-      }
-    }
-    return json
-  }
+  let yaml = shallowRef<YAML | null>(null)
+  yamlImport.then(yml => {
+    yaml.value = yml
+  })
 
   watchEffect(async (onInvalidate) => {
     if (!langLoaded.value || !yaml.value) {
@@ -69,7 +56,7 @@ export function useAstGrep() {
     }
     let invalidated = false
     // before async
-    const [src, json] = [state.source, buildRules()]
+    const [src, json] = [state.source, buildRules(yaml.value, state)]
     onInvalidate(() => invalidated = true)
     try {
       const [matches, fixed] = await doFind(src, json)
@@ -85,16 +72,25 @@ export function useAstGrep() {
       matchedHighlights.value = []
       matchedEnvs.value = []
     }
-    return
   })
-
   return {
-    state,
-    lang,
     matchedHighlights,
     matchedEnvs,
     rewrittenCode,
     ruleErrors,
+  }
+}
+
+// NB: this hook cannot be async since it sets up `provide`
+export function useAstGrep() {
+  const { state, lang, ...refs } = useSgState()
+  const langLoaded = useSetupParser(lang)
+  const results = useDoFind(langLoaded, state)
+
+  return {
+    state,
+    lang,
+    ...results,
     ...refs,
   }
 }
