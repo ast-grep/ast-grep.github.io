f623a445a42b0e943474501eab4d89d716d8a735
HerringtonDarkholme <2883231+HerringtonDarkholme@users.noreply.github.com>
2024-08-31 21:12:17 -0400

    feat: use general node for tree node

--- website/src/components/dump/TreeNode.vue
+++ website/src/components/dump/TreeNode.vue
@@ -2,7 +2,7 @@
 import { PropType } from 'vue'
 import { DumpNode, Pos, deepReactiveNode } from './dumpTree'
 import { showToast } from '../utils/Toast.vue'
-import { useHighlightNode } from './highlightNode'
+import GeneralNode from './GeneralNode.vue'
 
 const props = defineProps({
   node: {
@@ -29,14 +29,6 @@ let {
   isNamed,
 } = deepReactiveNode(props)
 
-const {
-  isWithin,
-  isTarget,
-  highlightNode,
-  expanded,
-  nodeRef,
-} = useHighlightNode(props)
-
 function copyKind(kind: string) {
   if (props.clickKind) {
     props.clickKind(kind)
@@ -52,79 +44,30 @@ function copyField(name: string) {
 </script>
 
 <template>
-  <div v-if="showUnnamed || isNamed || field" class="tree-node" ref="nodeRef" :class="{target: isTarget}">
-    <p class="click-area" @click.stop="expanded = !expanded" @mouseover="highlightNode">
-      <span
-        v-if="children.some(n => n.isNamed || n.field)"
-        class="toggle-sign"
-        :class="{expanded}"/>
+  <GeneralNode v-if="showUnnamed || isNamed || field"
+    :node="node"
+    :cursorPosition="cursorPosition"
+    :showToggle="children.some(n => n.isNamed || n.field)"
+  >
+    <template #info>
       <span v-if="isNamed" class="node-kind" @click.stop="copyKind(kind)">{{ kind }}</span>
       <span v-else class="node-text">{{ kind }}</span>
-      <span class="node-field" @click.stop="copyField(kind)">{{ field }}</span>
+      <span v-if="field" class="node-field" @click.stop="copyField(field)">{{ field }}</span>
       <span class="node-range">({{ start.row }}, {{start.column}})-({{ end.row }},{{ end.column }})</span>
-    </p>
-    <TreeNode
-      :showUnnamed="showUnnamed"
-      :node="child"
-      :cursorPosition="isWithin ? cursorPosition : null"
-      :clickKind="clickKind"
-      v-if="expanded"
-      v-for="child in children"
-    />
-  </div>
+    </template>
+    <template #children="{cursorPosition}">
+      <TreeNode
+        :showUnnamed="showUnnamed"
+        :node="child"
+        :cursorPosition="cursorPosition"
+        :clickKind="clickKind"
+        v-for="child in children"
+      />
+    </template>
+  </GeneralNode>
 </template>
 
-<style>
-.tree-node {
-  --yellow:    #bf8803;
-  --red:       #a31515;
-  --blue:      #006ab1;
-  --green:     #008000;
-  --black:     #002b36;
-}
-html.dark .tree-node {
-  --yellow:    #ce9178;
-  --red:       #f48771;
-  --blue:      #569cd6;
-  --green:     #608b4e;
-  --black:     #d4d4d4;
-}
-</style>
-
 <style scoped>
-.tree-node {
-  margin: 0 0 0 1em;
-  padding: 0 0 0 0.5em;
-  border-left: 1px dashed var(--vp-c-divider);
-  user-select: none;
-  transition: background-color ease-out .217s;
-}
-.click-area {
-  cursor: pointer;
-}
-.tree-node:has(> .click-area:hover) {
-  background-color: var(--theme-highlight4);
-}
-.tree-node.target {
-  background-color: var(--theme-highlight3);
-}
-/* fallback for browser without :has */
-.click-area:hover {
-  background-color: var(--theme-highlight4);
-}
-.toggle-sign {
-  color: var(--green);
-  display: inline-block;
-}
-.toggle-sign::before {
-  content: '+';
-}
-.toggle-sign.expanded {
-  color: var(--red);
-}
-.toggle-sign.expanded::before {
-  content: '-';
-}
 .node-kind {
   color: var(--blue);
   padding-left: 0.6em;
